5c86c687f46dfcc3f253053ebc1a915eac4d44ae
Fernando <fernandozimmermannmaciel@gmail.com>
2024-09-10 05:01:45 -0300

    add comments

--- meet-up-spot/src/main/java/com/meet_up_spot/MeetUpSpotApplication.java
+++ meet-up-spot/src/main/java/com/meet_up_spot/MeetUpSpotApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Great API to Work
 @SpringBootApplication
 public class MeetUpSpotApplication {
 	public static void main(String[] args) {
